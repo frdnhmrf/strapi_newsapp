8dbfb746f1a9b3ca86c774b4778c99d9966d557b
frdnhmrf <frdnhmrf@gmail.com>
2023-03-05 20:08:38 +0700

    initial commit

---
+++ src/api/trend/controllers/trend.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trend controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::trend.trend');
---
+++ src/api/trend/routes/trend.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trend router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::trend.trend');
